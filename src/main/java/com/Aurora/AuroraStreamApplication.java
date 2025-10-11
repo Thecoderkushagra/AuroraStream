003cb8aae18f217a591151ff24d4319db5e4a31e
Thecoderkushagra <officialkushagra241004@gmail.com>
2025-10-11 22:41:11 +0530

    first commit

---
+++ src/main/java/com/Aurora/AuroraStreamApplication.java
@@ -0,0 +1,13 @@
+package com.Aurora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuroraStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuroraStreamApplication.class, args);
+	}
+
+}
