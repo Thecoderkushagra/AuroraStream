571227bff74408c44cfe5bc41f959458ffcf38db
Thecoderkushagra <officialkushagra241004@gmail.com>
2025-11-02 15:49:50 +0530

    migration has started: mono-> micro

---
+++ ApiGateway-Aurora/src/main/java/com/TheCoderKushagra/ApiGatewayAuroraApplication.java
@@ -0,0 +1,13 @@
+package com.TheCoderKushagra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayAuroraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayAuroraApplication.class, args);
+	}
+
+}
--- src/main/java/com/Aurora/repository/VideoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Aurora.repository;
-
-import com.Aurora.entity.Video;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VideoRepository extends JpaRepository<Video, String> {
-}
