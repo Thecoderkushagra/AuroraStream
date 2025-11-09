53b4ee28f22973cfa1e1227eb342d36f327ce286
Thecoderkushagra <officialkushagra241004@gmail.com>
2025-11-10 03:06:21 +0530

    redis setup completed for user-service.

--- User-Service/src/main/java/com/TheCoderKushagra/UserServiceApplication.java
+++ User-Service/src/main/java/com/TheCoderKushagra/UserServiceApplication.java
@@ -2,6 +2,10 @@ package com.TheCoderKushagra;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @SpringBootApplication
 public class UserServiceApplication {
@@ -9,5 +13,16 @@ public class UserServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
+
+    @Bean
+    public RedisTemplate<String,String> redisTemplate(RedisConnectionFactory factory){
+        RedisTemplate<String,String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(factory);
+
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+        return redisTemplate;
+    }
 
 }
---
+++ User-Service/src/main/java/com/TheCoderKushagra/cache/UserCache.java
@@ -0,0 +1,38 @@
+package com.TheCoderKushagra.cache;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@Component
+public class UserCache {
+    @Autowired
+    private RedisTemplate<String,String> redisTemplate;
+
+    public void setUser(String key, Object value, long timeToLive){
+        try{
+            ObjectMapper objectMapper = new ObjectMapper();
+            String jsonValue = objectMapper.writeValueAsString(value);
+            redisTemplate.opsForValue().set(key,jsonValue,timeToLive, TimeUnit.SECONDS);
+        }catch (Exception e){
+            log.error("Exception",e);
+        }
+    }
+
+    public <T> T getUser(String key, Class<T> userClass){
+        try {
+            Object data = redisTemplate.opsForValue().get(key);
+            ObjectMapper mapper = new ObjectMapper();
+            assert data != null;
+            return mapper.readValue(data.toString(), userClass);
+        } catch (Exception e) {
+            log.error("Exception", e);
+            return null;
+        }
+    }
+}
